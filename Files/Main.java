69cb6583447e3c24d80378ab4f8c367a0f3480a2
Ahnaf Ahmed <61428434+ahnafIsCoding@users.noreply.github.com>
2024-04-30 23:38:43 -0400

    -u Add Scanner, Input methods, Custom formulas Scanner to take user input to override the default values of the number of games and win percentage. Throw exception for IO mismatch etc. Verfication of correct input values. Cutomized formulas and array sizes based on user inputs for #games and win%

--- Files/Main.java
+++ Files/Main.java
@@ -1,61 +1,119 @@
+/**
+ * Name: Ahnaf Ahmed
+ * Project #2 - Dynamic Programming Technique
+ * 
+ * CS323 Design and Analysis of Algorithms
+ * Spring 2024
+ * Prof. Anne Smith-Thompson
+ */
+
+import java.io.IOException;
 import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Main {
-   
-   public static final double p1 = .463;
-   public static final double p2 = .537;
 
-   public static void main(String[] args) {
+   public static DecimalFormat df = new DecimalFormat("00.00");
+   public static Scanner scanner = new Scanner(System.in);
+
+   public static void main(String[] args) throws IOException {
       
-      DecimalFormat df = new DecimalFormat("00.00");
+      System.out.println("Welcome to the World Series!");
+      System.out.println("-----------------------------");
+      System.out.println("It's the Mets vs the Yankees!");
+      System.out.println("-----------------------------");
 
-      long start = System.nanoTime();
-      double p = prob();
-      long end = System.nanoTime();
-      long time = end - start;
-      System.out.println("Mets win probability (memo): " +df.format(p*100)+ "% ["+time+"]");
-      start = System.nanoTime();
-      double pdp = prob2();
-      end = System.nanoTime();
-      time = end - start;
-      System.out.println("Mets win probability (dp): " +df.format(pdp*100)+ "% ["+time+"]");
-   }
-
-   public static double prob() {
-      double[][] memo = new double[5][5];
-      for (int i = 0; i < 5; i++) {
-         for (int j = 0; j < 5; j++) {
-            memo[i][j] = -1;
-         }
-      }
-      for (int i = 0; i < 5; i++) {
-         memo[4][i] = 1; // mets win all 4
-         memo[i][4] = 0; // yankees win all 4
-      }
-      return prob(memo,0,0);
+      int n = bestOf();
+      double p = winPercent();
+      
+      double mets_win = probability(n,p);
+      System.out.println("Mets win probability: " +df.format(mets_win*100)+ "%");
    }
    
-   public static double prob(double[][] memo, int i, int j) {
-      if (memo[i][j] != -1) return memo[i][j];
-      else {
-         double result;
-         result = p1 * prob(memo, i+1, j) + p2 * prob(memo, i, j+1);
-         memo[i][j] = result;
-         return result;
+   /**
+    * 
+    * @param n
+    * @param p
+    * @return
+    */
+   public static double probability(int n, double p) {
+      int w = Math.ceilDiv(n, 2) + 1;
+      if (n%2 == 0) w++;
+      double[][] table = new double[w][w];
+      for (int i = 0; i < w; i++) {
+         table[w-1][i] = 1;
+         table[i][w-1] = 0;
       }
-   }
-   
-   public static double prob2() {
-      double[][] dp = new double[5][5];
-      for (int i = 0; i < 5; i++) {
-         dp[4][i] = 1; // mets win all 4
-         dp[i][4] = 0; // yankees win all 4
-      }
-      for (int i = 3; i >= 0; i--) {
-         for (int j = 3; j >= 0; j--) {
-            dp[i][j] = p1 * dp[i+1][j] + p2 * dp[i][j+1];
+      for (int i = w-2; i >= 0; i--) {
+         for (int j = w-2; j >= 0; j--) {
+            table[i][j] = p * table[i+1][j] + (1-p) * table[i][j+1];
          }
       }
-      return dp[0][0];
+      // print table
+//      for (int i = 0; i < w; i++) {
+//         for (int j = 0; j < w; j++) {
+//            System.out.print(df.format(table[i][j])+"\t");
+//         }
+//         System.out.println();
+//      }
+      return table[0][0];
+   }
+   
+   /**
+    * 
+    * @return
+    * @throws IOException
+    */
+   public static int bestOf() throws IOException {
+      int choice = -1;
+      do {
+         System.out.println("Would you like to change the default number of games (7)?");
+         System.out.print("Enter '1' for 'yes' or '0' for 'no'\n> ");
+         choice = scanner.nextInt();
+         if (choice != 1 && choice != 0)
+            System.out.println("Error! Please enter a valid input!");
+         System.out.println("-----------------------------");
+      } while (choice != 1 && choice != 0);
+      if (choice == 1) {
+         int num = -1;
+         do {
+            System.out.print("Enter the number of games\n> ");
+            num = scanner.nextInt();
+            if (num <= 0)
+               System.out.println("Error! Please enter a positive integer!");
+            System.out.println("-----------------------------");
+         } while (num <= 0);
+         return num;
+      }
+      else return 7;
+   }
+
+   /**
+    * 
+    * @return
+    * @throws IOException
+    */
+   public static double winPercent() throws IOException {
+      int choice = -1;
+      do {
+         System.out.println("Would you like to change the default probability (46.3% / 0.463) of the Mets winning a single game?");
+         System.out.print("Enter '1' for 'yes' or '0' for 'no'\n> ");
+         choice = scanner.nextInt();
+         if (choice != 1 && choice != 0)
+            System.out.println("Error! Please enter a valid input!");
+         System.out.println("-----------------------------");
+      } while (choice != 1 && choice != 0);
+      if (choice == 1) {
+         double num = -1.0;
+         do {
+            System.out.print("Enter the new probability as a decimal number\n> ");
+            num = scanner.nextDouble();
+            if (num <= 0)
+               System.out.println("Error! Please enter a positive decimal number!");
+            System.out.println("-----------------------------");
+         } while (num <= 0);
+         return num;
+      }
+      else return 0.463;
    }
 }
